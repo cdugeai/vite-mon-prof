5e4580b40d618e2ec4702f2d67d93c1aaed6cd4c
Colin Dugeai <colin.dugeai@gmail.com>
2023-06-02 21:39:14 +0200

    plugin.firefox : comment code

--- src/plugin/firefox/src/main.js
+++ src/plugin/firefox/src/main.js
@@ -9,24 +9,33 @@ document.body.style.border = "5px solid red";
 
 console.log("Script runs")
 
+/**
+ * Get dom element from its xpath
+ * @param path xpath of the element
+ * @return {Node} Dom element
+ */
 function getElementByXpath(path) {
 	return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
 
-
+// Get the date
 let dateXpath = '//*[@id="IE.Identite.collection.g8.cellule_Edit"]'
 let date_ = getElementByXpath(dateXpath).innerText
 
-//let all_li = document.getElementsByTagName("li");
+// Get all li elements
+let all_li = document.getElementsByTagName("li");
+// Set border to '1px green'
+Array.prototype.slice.call(all_li).forEach(e => {
+	e.style.border = '1px solid green'
+})
 
-//all_li[65].style.border = '5px solid red'
-
+// Set onclick on the li
 $('li').on('click', function() {
-	//$('.whitebg').removeClass('whitebg');
-	//$(this).addClass('whitebg');
 	console.log(this);
+	// First, set border to '5px red'
 	this.style.border = '5px solid red' ;
+	// Second, get information about the course
 	let heure = this.children[0].innerText;
 	let matiere_li = this.children[2];
 	let matiere = {
@@ -38,5 +47,6 @@ $('li').on('click', function() {
 	console.log({heure})
 	console.log({matiere})
 
+	// Finally, display information about selected course to the user
 	alert(`Signaler une absence pour le cours suivant ? \n - le ${date_} \n - ${matiere.libelle} \n - avec ${matiere.prof} \n - en salle ${matiere.salle}`)
 });
